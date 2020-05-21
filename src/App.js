b3fae14d35f69898036008e988daf13b089b4635
ThongD <thong.do@gumiviet.com>
2020-05-21 13:51:24 +0700

    first stash

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Hello git</h1>
+<<<<<<< Updated upstream
       <h4>This is my second stash</h4>
+=======
+      <h4>This is my first stash</h4>
+>>>>>>> Stashed changes
     </div>
   );
 }
