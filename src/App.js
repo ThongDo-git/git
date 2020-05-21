6bc6f4b0194bc40f039debb37dd9277f4833fed0
ThongD <thong.do@gumiviet.com>
2020-05-21 14:18:55 +0700

    form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello git</h1>
-      <h4>This is my first stash</h4>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: ''
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ email: e.target.value })
+  }
+
+  handleSubmit() {
+    alert(this.state.email)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='email'>Email: </label>
+          <input type='text' value={this.state.email} onChange={this.handleChange} />
+          <input type='submit' value='Submit' />
+        </form>
+      </div>
+    )
+  }
 }
 
 export default App;
